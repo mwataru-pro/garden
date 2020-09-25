079fd458cb9606ecfb527d0ff3caf07400cddb86
mwataru-pro <m1130wata@gmail.com>
2020-09-25 14:16:29 +0000

    ヘッダーのリンクとログイン時のリダイレクト先修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_in_path_for(resource)
     case resource
     when Admin
-      admin_admins_path
+      admin_top_path
     when Customer
       root_path
     end
@@ -13,7 +13,7 @@ before_action :configure_permitted_parameters, if: :devise_controller?
   def after_sign_up_path_for(resource)
     case resource
     when Admin
-      admin_admins_path
+      admin_top_path
     when Customer
       root_path
     end
