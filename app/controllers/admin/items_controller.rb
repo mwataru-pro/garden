294e5264d803ef05ab2d0817790c65fc0eb4eb92
mwataru-pro <m1130wata@gmail.com>
2020-09-26 02:40:53 +0000

    商品登録画面の編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,35 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(params)
+    if @item.save
+      redirect_to admin_item_path(@item)
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
-  def new
+  def update
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id,:item_name,:explanation,:price,:sale_status,:image)
   end
 end
