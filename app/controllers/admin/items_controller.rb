decab7b9993c2d3b40be4f42f038196887271dc3
mwataru-pro <m1130wata@gmail.com>
2020-09-27 06:53:07 +0000

    商品登録画面の編集

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -16,7 +16,7 @@ class Admin::ItemsController < ApplicationController
     if @item.save
       redirect_to admin_item_path(@item)
     else
-      render "new"
+      render "new" 
     end
   end
 
