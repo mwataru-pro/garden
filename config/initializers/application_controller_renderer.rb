6586f976de7d00c459edc3c7ecfcad94efd5c56c
mwataru-pro <m1130wata@gmail.com>
2020-10-01 14:57:43 +0000

    商品詳細画面の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ece68bf9ebdd14c1e2995d79d02c6f6369f89ef012823586d4d7fafcdf42cb26d3fe6afae6ffe84a0fc3c20b732425906aa5ab7e814469e7c6e19bfb7ef995ac'
