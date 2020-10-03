134ee2b02af7b54587e2a27695cc96ca96fe3c44
mwataru-pro <m1130wata@gmail.com>
2020-10-03 02:06:02 +0000

    注文ステータス調整

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,2 +1,19 @@
 class Admin::OrderItemsController < ApplicationController
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order = @order_item.order
+    @order_item.update(order_item_params)
+
+    if @order_item.making_status == "製作中"
+      @order.update(order_status: 2)
+    elsif @order.order_items.count ==  @order.order_items.where(making_status: 3).count
+      @order.update(order_status: 3) #注文ステータスを「発送準備中]に更新
+    end
+    redirect_to admin_order_path(@order_item.order) #注文詳細に遷移
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:making_status)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,10 +9,20 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_items = @order.order_items
+    if @order.update(order_params)
+      if @order.order_status == "入金確認" # order_statusが入金確認に更新されたら
+        @order_items.update_all(making_status: 1) # 制作待ちに変更する処理
+        redirect_to admin_order_path(@order)
+      end
+    else
+      render "show"
+    end
   end
 
   private
   def order_params
-    prams.require(:order).permit(:order_status)
+    params.require(:order).permit(:order_status)
   end
 end
